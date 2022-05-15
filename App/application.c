d9834a7f68e62868737d00d7a4f9ee88f97185ac
Alex.M <alex.m.wrk@gail.com>
2022-05-15 16:28:24 +0300

    add logs input func

--- App/application.c
+++ App/application.c
@@ -1,11 +1,48 @@
 #include "main.h"
 #include "application.h"
 #include "log_libs.h"
+#include "config.h"
+
+void ReadCommandBuf(void)
+{
+    static char buff[32];
+    static uint8_t pos = 0;
+    int32_t key = LogLibsGetChar();
+    if (key < 0)
+    {
+        // continue;
+        return;
+    }
+    if ((char)key == '\n')
+    {
+        if (pos == 0)
+            // continue;
+            return;
+        buff[pos] = '\0';
+        pos = 0;
+        // new string
+    }
+    else if (pos < (sizeof(buff) - 1))
+    {
+        buff[pos++] = key;
+    }
+}
+
+void indication(void)
+{
+    static uint32_t prev_tick = 0;
+    if (HAL_GetTick() > (prev_tick + SYSTEM_LED_BLINK_PEROD))
+    {
+        HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+        prev_tick = HAL_GetTick();
+    }
+}
+
 void application(void)
 {
     while (1)
     {
-        HAL_Delay(1000);
-        HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+        ReadCommandBuf();
+        indication();
     }
 }
---
+++ App/config.h
@@ -0,0 +1,6 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SYSTEM_LED_BLINK_PEROD 500 // ms
+
+#endif // !CONFIG_H
